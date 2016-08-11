7bdb468fa704dc120d283dff793b7f711d1ed168
jeff <dcirplan@gmail.com>
2016-08-11 16:30:43 +0800

    add test

---
+++ test/index.js
@@ -0,0 +1,9 @@
+var assert = require('assert');
+
+describe('test', function() {
+	describe('test', function() {
+		it('should return true', function() {
+			assert.equal(true, true);
+		});
+	});
+});
